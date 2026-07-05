60ab254756da161debbb55136a9611797f56bb82
reliablecomputech1-stack <amitwork0099@gmail.com>
2026-07-05 20:44:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ document.addEventListener("DOMContentLoaded", () =>
 {
     const form = document.getElementById("customerForm");
     const message = document.getElementById("message");
+    document.getElementById("photo").addEventListener("change", function () {
+    const fileNameSpan = document.getElementById("fileName");
+
+    if (this.files.length > 0) {
+        fileNameSpan.textContent = this.files[0].name;
+    } else {
+        fileNameSpan.textContent = "No file selected";
+    }
+});
 
     // =======================
     // ADD CUSTOMER
