f0fc97a4cfc342b896f5a8d62927ba17c9fdb460
reliablecomputech1-stack <amitwork0099@gmail.com>
2026-07-06 22:26:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -236,11 +236,11 @@ row.querySelectorAll(".name, .contact, .description, .status, .estimate")  .forE
     const row = e.target.closest("tr");
     const id = row.dataset.id;
 const updated = {
-    name: row.querySelector(".name").innerText,
-    contact: row.querySelector(".contact").innerText,
-    description: row.querySelector(".description").innerText,
-    status: row.querySelector(".status").innerText,
-    estimate: row.querySelector(".estimate").innerText
+  name: row.querySelector(".name").innerText,
+  phone: row.querySelector(".contact").innerText,
+  description: row.querySelector(".description").innerText,
+  status: row.querySelector(".status").innerText,
+  estimate: row.querySelector(".estimate").innerText
 };
 
     const { error } = await sb.from("customers").update(updated).eq("id", id);
